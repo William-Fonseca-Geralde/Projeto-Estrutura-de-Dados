f111d1002d9f839e6209ee57d1a5487d5c4ccd1a
William-Fonseca-Geralde <127157689+William-Fonseca-Geralde@users.noreply.github.com>
2023-06-23 16:32:12 -0300

    Add files via upload

---
+++ Prova_ED/main.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include "urna.h"
+using namespace std;
+
+int main() {
+
+  string nome;
+  char resp;
+  bool err;
+  long long int titu;
+  int valor, opcao;
+
+  do {
+    system("cls");
+    cout << "****************A FESTA DA DEMOCRACIA****************\n" << '\n';
+    cout << "\t1 - Cadastro de Candidatos;\n";
+    cout << "\t2 - Cadastro de Eleitores;\n";
+    cout << "\t3 - Registro de Voto;\n";
+    cout << "\t4 - Sair do Programa.\n";
+    cout << "\nDigite uma das opcoes: ";
+    cin >> opcao;
+
+    system("cls");
+    switch (opcao) {
+    case 1:
+      do {
+        system("cls");
+        cout << "Digite o nome desse candidato: ";
+        getline(cin >> ws, nome);
+        cout << "Digite o seu numero: ";
+        cin >> valor;
+
+        inserirCandidato(valor, nome);
+
+        cout << "\nDeseja adicionar mais um candidato? (s / n) ";
+        cin >> resp;
+      } while (resp == 's' || resp == 'S');
+      system("cls");
+      
+      cout << "Deseja remover algum candidato? (s / n) ";
+      cin >> resp;
+
+      while (resp == 's' || resp == 'S') {
+        system("cls");
+        cout << "Digite o numero do candidato que quer ser removido: ";
+        cin >> valor;
+        err = removerCandidato(valor);
+        if (err == true) {
+          resp = 's';
+          cout << "Nao encontrado\n";
+          cout << "Deseja remover outro candidato? (s / n) ";
+          cin >> resp;
+        } else {
+          cout << "Removido\n";
+          cout << "Deseja remover mais algum candidato? (s / n) ";
+          cin >> resp;
+        }
+      }
+
+      arq.open("ListaCandidatos.txt");
+      mostrarCandidatos();
+      arq.close();
+      
+    break;
+    case 2:
+      do {
+        system("cls");
+        cout << "Digite o nome do eleitor: ";
+        getline(cin >> ws, nome);
+        cout << "E o seu titulo de eleitor: ";
+        cin >> titu;
+
+        inserirEleitor(titu, nome);
+
+        cout << "\nDeseja adicionar mais eleitores? (s / n) ";
+        cin >> resp;
+      } while (resp == 's' || resp == 'S');
+      system("cls");
+      
+      arq.open("ListaEleitoresAptos.txt");
+      mostrarEleitores();
+      arq.close();
+    break;
+    case 3:
+      system("cls");
+      if (estaVazioCandidato()) {
+        cout << "Nao existe candidatos nessa eleicao\n";
+        cout << "Por favor, cadastrar mais candidatos\n";
+        system("Pause");
+      } else if (umCandidato()) {
+        cout << "Existe apenas um unico candidato nessa eleicao\n";
+        cout << "Por favor, cadastrar, pelo menos, mais um candidatos\n";
+        system("Pause");
+      } else if (estaVazioEleitor()) {
+        cout << "Nao exite eleitor nessa sala\n";
+        system("Pause");
+      } else if (verificarQuantEleitor()){
+        cout << "Nao tem eleitores o suficiente para essa sala\n";
+        cout << "Por motivos de prevenir faltas dos mesmo, por favor, cadastrar mais eleitores\n";
+        system("Pause");
+      } else {
+        do {
+          system("cls");
+          cout << "Digite o titulo do eleitor para verificar se esta apto ou nao\n";
+          cin >> titu;
+
+          err = verificaApto(titu);
+          if (err == true) {
+            cout << "O eleitor nao esta apto\n";
+            cout << "\nExistem eleitores na fila? (s / n)";
+            cin >> resp;
+          } else {
+            do {
+              cout << "Para qual candidato ele votou? (apenas o numero dele) ";
+              cin >> valor;
+
+              err = inserirVoto(valor);
+
+              if (err == true){
+                cout << "Candidato nao encontrado\n";
+                system("Pause");
+                valor = 0;
+              } else {
+                cout << "Voto inserido...\n";
+                cout << "Existem mais eleitores na fila? (s / n) ";
+                cin >> resp;
+              }
+            } while (valor == 0);
+          }
+        } while (resp == 's' || resp == 'S');
+
+        arq.open("RelatorioEleicao.txt");
+        mostrarVencedor();
+        arq.close();
+      }
+    break;
+    case 4:
+      system("cls");
+      cout << "Saindo do Programa...";
+    break;
+    default:
+      break;
+    }
+  } while (opcao != 4);
+
+  return 0;
+}
---
+++ Prova_ED/urna.h
@@ -0,0 +1,300 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+ofstream arq;
+
+typedef struct noCand {
+  int num;
+  string nome;
+  struct noCand *prox, *ante;
+} *listaCand;
+
+
+static listaCand cabecaC = NULL;
+static listaCand raboC = NULL;
+static listaCand tempC = NULL;
+
+typedef struct noVoto {
+  int numCand, quantVotos = 0;
+  string nomeCand;
+  struct noVoto *prox, *ante;
+} *listaVoto;
+
+
+static listaVoto cabecaV = NULL;
+static listaVoto raboV = NULL;
+static listaVoto tempV = NULL;
+
+typedef struct noEleit {
+  long long int titulo;
+  string nome;
+  struct noEleit *prox, *ante;
+} *listaEleit;
+
+
+static listaEleit cabecaE = NULL;
+static listaEleit raboE = NULL;
+static listaEleit tempE = NULL;
+
+int numTotalEleit, numTotalCandidatos;
+
+/*Candidatos*/
+bool estaVazioCandidato();
+bool umCandidato();
+void inserirCandidato(int numero, string nome);
+bool removerCandidato(int numero);
+void mostrarCandidatos();
+
+/*Eleitores*/
+bool estaVazioEleitor();
+void inserirEleitor(long long int titu, string nome);
+bool verificaApto(long long int titu);
+void mostrarEleitores();
+bool verificarQuantEleitor();
+
+/*Votos*/
+bool inserirVoto(int numero);
+void mostrarVencedor();
+
+bool estaVazioCandidato(){
+  if (cabecaC == NULL)
+    return true;
+  else
+    return false;
+}
+
+bool umCandidato(){
+  if (cabecaC == raboC)
+    return true;
+  else
+    return false;
+}
+
+void inserirCandidato(int numero, string nome){
+  tempC = new noCand;
+
+  tempC->nome = nome;
+  tempC->num = numero;
+
+  tempC->prox = NULL;
+  tempC->ante = NULL;
+  if (cabecaC == NULL) {
+    cabecaC = tempC;
+  } else {
+    raboC->prox = tempC;
+    tempC->ante = raboC;
+  }
+  raboC = tempC;
+  tempC = NULL;
+
+  delete tempC;
+}
+
+bool removerCandidato(int numero){
+  bool erro;
+  noCand *removC = cabecaC;
+
+  while (removC->prox != NULL && removC->num != numero) {
+    removC = removC->prox;
+  }
+
+  if (removC->num != numero) {
+    erro = true;
+  } else if (removC->num == numero) {
+    if (removC != cabecaC && removC != raboC) {
+      removC->prox->ante = removC->ante;
+      removC->ante->prox = removC->prox;
+    } else if (removC == cabecaC) {
+      cabecaC = removC->prox;
+      removC->prox->ante = NULL;
+    } else if (removC == raboC) {
+      raboC = removC->ante;
+      removC->ante->prox = NULL;
+    }
+
+    removC->ante = NULL;
+    removC->prox = NULL;
+
+    if (raboC == cabecaC) {
+      delete raboC;
+      delete cabecaC;
+    }
+    
+    erro = false;
+  }
+
+  delete removC;
+
+  return erro;
+}
+
+void mostrarCandidatos(){
+  numTotalCandidatos = 0;
+
+  arq << "***************LISTA DE CANDIDATOS***************\n";
+  arq << endl;
+
+  for (tempC = cabecaC; tempC != NULL; tempC = tempC->prox) {
+    arq << "Nome: " << tempC->nome << '\n';
+    arq << "Numero dele: " << tempC->num << '\n';
+    arq << '\n';
+    numTotalCandidatos++;
+  }
+
+  arq << "Tem " << numTotalCandidatos << " candidatos";
+}
+
+
+bool estaVazioEleitor(){
+  if (cabecaE == NULL)
+    return true;
+  else
+    return false;
+}
+
+void inserirEleitor(long long int titu, string nome){
+  tempE = new noEleit;
+
+  tempE->nome = nome;
+  tempE->titulo = titu;
+
+  tempE->prox = NULL;
+  tempE->ante = NULL;
+  if (cabecaE == NULL) {
+    cabecaE = tempE;
+  } else {
+    raboE->prox = tempE;
+    tempE->ante = raboE;
+  }
+  raboE = tempE;
+  tempE = NULL;
+
+  delete tempE;
+}
+
+bool verificaApto(long long int titu){
+  bool apto;
+
+  for (tempE = cabecaE; tempE != NULL; tempE = tempE->prox) {
+    if (titu != tempE->titulo && tempE->prox == NULL) {
+      apto = true;
+      break;
+    } else if (titu == tempE->titulo) {
+      apto = false;
+      break;
+    }
+  }
+
+  return apto;
+}
+
+void mostrarEleitores(){
+  numTotalEleit = 0;
+
+  arq << "***************LISTA DE ELEITORES***************\n";
+  arq << endl;
+
+  for (tempE = cabecaE; tempE != NULL; tempE = tempE->prox) {
+    arq << "Nome: " << tempE->nome << '\n';
+    arq << "Titulo: " << tempE->titulo << '\n';
+    numTotalEleit++;
+    arq << '\n';
+  }
+
+  arq << "Tem " << numTotalEleit << " Eleitores";
+}
+
+bool verificarQuantEleitor(){
+  if (numTotalCandidatos <= numTotalEleit/2.5)
+    return true;
+  else
+    return false;
+}
+
+
+bool inserirVoto(int numero){
+  bool encontr;
+
+  for ( tempC = cabecaC; tempC != NULL; tempC = tempC->prox) {
+    if (tempC->num == numero) {
+      encontr = false;
+
+      if (cabecaV == NULL) {
+        tempV = new noVoto;
+
+        cabecaV = tempV;
+
+        tempV->nomeCand = tempC->nome;
+        tempV->numCand = tempC->num;
+        tempV->quantVotos++;
+
+        tempV->prox = NULL;
+        tempV->ante = NULL;
+      } else {
+        for (tempV = cabecaV; tempV != NULL; tempV = tempV->prox) {
+          if (tempV->numCand == numero) {
+            tempV->quantVotos++;
+            if (tempV != cabecaV) {
+              tempV->prox->ante = tempV->prox;
+              tempV->ante->prox = tempV->ante;
+              tempV->prox = cabecaV;
+              tempV->ante = NULL;
+              cabecaV = tempV;
+            } else if (tempV == raboV && tempV != cabecaV) {
+              tempV->ante->prox = NULL;
+              raboV = tempV->ante;
+              tempV->ante = NULL;
+              tempV->prox = cabecaV;
+            }
+            break;
+          } else if (tempV->numCand != numero && tempV->prox == NULL) {
+            tempV = new noVoto;
+
+            tempV->nomeCand = tempC->nome;
+            tempV->numCand = tempC->num;
+            tempV->quantVotos++;
+
+            raboV->prox = tempV;
+            tempV->ante = raboV;
+            break;
+          }
+        }
+      }
+      raboV = tempV;
+
+      tempV = NULL;
+
+      delete tempV;
+
+      break;
+    } else if (tempC->num != numero && tempC->prox == NULL) {
+      encontr = true;
+      break;
+    }
+  }
+  
+  return encontr;
+}
+
+void mostrarVencedor(){
+  int numTotalVotos = 0;
+
+  arq << "***************RELATORIO DA ELEICAO***************\n";
+  arq << endl;
+
+  for (tempV = cabecaV; tempV->prox != NULL; tempV = tempV->prox) {
+    arq << "Nome do Candidato: " << tempV->nomeCand << "\tNumero: " << tempV->numCand << '\n';
+    arq << "Quantidade de Votos: " << tempV->quantVotos << '\n';
+    numTotalVotos++;
+    arq << '\n';
+  }
+  
+  arq << endl << endl;
+  arq << "Quantidade de votos: " << numTotalVotos << '\n';
+  if (numTotalEleit != numTotalVotos) {
+    arq << "Faltaram " << numTotalEleit - numTotalVotos << " Eleitores nessa eleicao" << '\n';
+  } else {
+    arq << "Todos os Eleitores compareceram nessa eleicao";
+  }
+}
